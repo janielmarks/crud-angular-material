c801103bd72442a979fa78cd7f3bd550ff366249
Janiel Marks <janielmarques@icloud.com>
2026-08-28 09:14:33 -0300

    feat: initial CRUD application with angular material design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet, RouterLink} from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule],
+
+templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
